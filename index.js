f25a1c48bd46b43914447784f36ca23afc595c6d
Hamjy09 <hamjyjoker970@gmail.com>
2024-08-28 06:35:23 -0700

    Ticket Counter App

---
+++ index.js
@@ -0,0 +1,20 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save() {
+
+console.log(count)
+    
+}
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = count
+}
